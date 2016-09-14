72781c5046d66824d1c2f8d81ce8734c74fff178
shaddo jaddo <iiedefault@gmail.com>
2016-09-14 23:41:36 +0900

    Initial Commit

---
+++ asdf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asdf
+//
+//  Created by jaddoshaddo on 2016/09/14.
+//  Copyright © 2016年 jaddoshaddo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
